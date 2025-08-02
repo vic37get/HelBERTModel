11eee9bc0ff45fb187f53f444a4a15f1ccd6370c
Victor <victorribeiro172@gmail.com>
2025-08-02 10:09:54 -0300

    remoções de códigos inutilizados

--- Codigos/LLMs/LangChainProjeto/Tradutor/cliente.py
+++
@@ -1,5 +0,0 @@
-from langserve import RemoteRunnable
-
-chain_remota = RemoteRunnable("http://localhost:8000/tradutor")
-texto = chain_remota.invoke({"language": "inglês", "texto": "Eu consegui aprender os primeiros passos com LangChain."})
-print(texto)
